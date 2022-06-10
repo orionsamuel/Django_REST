f881b64079f093a003374029426aea9c8fea6840
Samuel Oliveira <s.oliveira.15@hotmail.com>
2022-06-10 12:20:27 -0300

    Finalizando com as APIs

--- api_todo/app/urls.py
+++ api_todo/app/urls.py
@@ -1,8 +1,15 @@
-from app.views import TodoListAndCreate, TodoDetailChangeAndDelete
+from app.views import TodoViewSet 
+'''TodoListAndCreate, TodoDetailChangeAndDelete'''
 
-from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'', TodoViewSet)
+urlpatterns = router.urls
+
+'''from django.urls import path
 
 urlpatterns = [
     path('', TodoListAndCreate.as_view()),
     path('<int:pk>/', TodoDetailChangeAndDelete.as_view()),
-]
+]'''
